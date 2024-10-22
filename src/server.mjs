c4119fe25686047f5ca21aedd3817c191b1a8846
EdwinDev6 <edwinmendoza0620@gmail.com>
2024-10-22 11:15:38 -0400

    refactor: update server.mjs to include express-session and session settings

--- src/server.mjs
+++ src/server.mjs
@@ -1,15 +1,44 @@
-import express from 'express'
-import helmet from 'helmet';
+import express from "express";
+import helmet from "helmet";
+import session from "express-session";
+import crypto from "crypto";
+import { SESSION_SECRET } from "./config.mjs";
+import { ENVIRONMENT } from "./config.mjs";
+import { loginRouter } from "./routes/login.routes.mjs";
+import { homeRouter }  from "./routes/home.routes.mjs";
 
 const app = express();
 
-app.use(express.json())
+const sessionSettings = {
+    genid: () => { return crypto.randomUUID(); },
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+    },
+
+}
+
+if (ENVIRONMENT === "production") {
+    sessionSettings.cookie = {
+        secure: true,
+    };
+}
+
+app.use(express.json());
+
+app.use(session(sessionSettings
+));
 
 //Helper to send http headers appropiately
-app.use(helmet())
+app.use(helmet());
 
 //Reduces fingerprinting
-app.disable("x-powered by")
+app.disable("x-powered by");
 
+// Add routes
+app.use("/api", loginRouter);
+app.use("/api", homeRouter);
 
-export default app
+export default app;
